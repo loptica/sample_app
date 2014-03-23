0e419057d48455cd02fb92bf2a49fcd36ba392e3
antonija <antonija1023@gmail.com>
2014-03-23 11:04:42 +0100

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title)
+  	base_title = "Sample App"
+  	if page_title.empty?
+  	 base_title # nama je page_title empty
+  	else
+  		"#{base_title} | #{page_title}"
+  	end
+  end 
 end
