9ad03cedda04ef6be8487780dca74b2e36a53da1
antonija <antonija1023@gmail.com>
2014-03-27 19:59:37 +0100

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+  resources :users
+
+  root 'static_pages#home'
   match "/help",    to: 'static_pages#help',    via: 'get'
   match "/about",   to: 'static_pages#about',   via: 'get'
   match "/contact", to: 'static_pages#contact', via: 'get'
@@ -9,7 +11,7 @@ SampleApp::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
- root 'static_pages#home'
+ 
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -52,7 +52,7 @@ describe "when name is not present" do
  describe "when user's email is not valid" do
    it "should be invalid" do
    	 addresses =  %w[user@foo,com user_at_foo.org example.user@foo.
-                     foo@bar_baz.com foo@bar+baz.com]
+                     foo@bar_baz.com foo@bar+baz.com foo@bar..com]
    	 addresses.each do |invalid_address|
    	   @user.email = invalid_address
    	   expect(@user).not_to be_valid
@@ -73,7 +73,7 @@ describe "when name is not present" do
  describe "when email address is already taken" do
  	before do
  		user_with_same_email = @user.dup
- 		user_with_same_email.email = @user.email.upcases
+ 		user_with_same_email.email = @user.email.upcase
  		user_with_same_email.save 
  	end
 
@@ -101,4 +101,14 @@ describe "when name is not present" do
    before {@user.password = @user.password_confirmation = "a" * 5}
    it {should be_invalid}
  end
-``
+
+ describe "email address with mixed case" do
+   let(:mixed_case_email) {"Foo@ExAMPle.CoM"}
+
+   it "should be saved as all all lower-case" do
+     @user.email = mixed_case_email
+     @user.save
+     expect(@user.reload.email).to eq mixed_case_email.downcase
+   end
+ end
+end
