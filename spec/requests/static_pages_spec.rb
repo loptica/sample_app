cfe417ed741bf228e7ca3dcee017291201f77237
antonija <antonija1023@gmail.com>
2014-04-12 22:06:26 +0200

    Add user microposts

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -24,16 +24,17 @@ describe "Static pages" do
         FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
         sign_in user
         visit root_path
-      end
+      end # od before
 
       it "should render the user's feed" do
         user.feed.each do |item|
           expect(page).to have_selector("li##{item.id}", text: item.content)
-        end
-      end
-    end #sgined in u
-  end # home page
+        end # each
+      end # should render
+    end #signed in user
+end
 
+  
   describe "Help page" do
     before { visit help_path }
 
